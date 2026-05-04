ac8884e221560f2801772b6dece0f645e3a857a3
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2026-05-04 14:14:22 +0700

    feat: update the app favicon logo

--- booking-personal-trainer-web/src/app/layout.tsx
+++ booking-personal-trainer-web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import "flatpickr/dist/flatpickr.css";
@@ -11,6 +12,14 @@ import Auth0UserProvider from "@/components/providers/Auth0UserProvider";
 import Auth0BackendSync from "@/components/providers/Auth0BackendSync";
 import Auth0LinkingBlocker from "@/components/providers/Auth0LinkingBlocker";
 import { auth0 } from "@/lib/auth0";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/images/logo/hdevfit-favicon.svg",
+    shortcut: "/images/logo/hdevfit-favicon.svg",
+    apple: "/images/logo/hdevfit-favicon.svg",
+  },
+};
 
 const outfit = Outfit({
   subsets: ["latin"],
